a6db195b338cb70923f8b977a349260e7567037d
Belal <belal@belal.wind3.hub>
2025-11-27 20:57:33 +0100

    Update all files

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import os
 from glob import glob
-=======
->>>>>>> 2bce28df3d3ec3fcaab70fd3c98c397c6bef0b63
 from setuptools import find_packages, setup
 
 package_name = 'assignment1_rt'
@@ -15,17 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-<<<<<<< HEAD
-        # Include all launch files
+        # Include all launch files from the launch directory
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-=======
->>>>>>> 2bce28df3d3ec3fcaab70fd3c98c397c6bef0b63
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='belal',
     maintainer_email='belal@todo.todo',
-    description='TODO: Package description',
+    description='Assignment 1 RT: UI, Distance Monitor, and Spawner',
     license='TODO: License declaration',
     extras_require={
         'test': [
@@ -34,12 +28,10 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            # FORMAT: 'executable_name = package_name.file_name:main'
             'ui_node = assignment1_rt.ui_node:main',
-            'turtle_spawn = assignment1_rt.turtle_spawn:main',
-<<<<<<< HEAD
-            'distance_monitor = assignment1_rt.distance_monitor:main', # Added new node
-=======
->>>>>>> 2bce28df3d3ec3fcaab70fd3c98c397c6bef0b63
+            'turtle_spawn = assignment1_rt.turtle_spawn:main', 
+            'distance_monitor = assignment1_rt.distance_monitor:main',
         ],
     },
 )
